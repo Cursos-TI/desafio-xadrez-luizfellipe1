84bab03bb78fbe1da5bdf6b19ae0c3d43962446f
luizfellipe1 <fellipe.libanin@gmail.com>
2025-07-02 20:58:58 +0000

    Novo for

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
-   printf("desafio xadrez!\n");
-   printf("Novo commit\n");
+   printf ("desafio xadrez!\n");
+   printf ("Novo commit\n");
+   for (int i = 0; i < 5; i++ )
+   printf ("Direita\n");
 }
